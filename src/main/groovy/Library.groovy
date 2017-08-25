2cfa563adb748a62229346b49135cbfe0fd42829
Juri Surakka <juri.surakka@digia.com>
2017-08-25 12:07:43 +0300

    Add failing test.

--- src/main/groovy/Library.groovy
+++ src/main/groovy/Library.groovy
@@ -2,4 +2,7 @@
  * Created by jusurakk on 8/25/17.
  */
 class Library {
+
+    String label
+
 }
--- src/test/groovy/LibraryTest.groovy
+++ src/test/groovy/LibraryTest.groovy
@@ -1,5 +1,15 @@
+import org.junit.Test
+
 /**
  * Created by jusurakk on 8/25/17.
  */
 class LibraryTest {
+
+    @Test
+    void testLibraryInit() {
+        Library library = new Library(label: 'Numibia')
+
+        assert library.label == 'numibia'
+
+    }
 }
